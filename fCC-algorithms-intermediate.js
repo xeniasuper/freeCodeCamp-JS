369190bc90e9676412a0ced75b79900ccad0feb9
xeniasuper <ichmalegern@gmail.com>
2019-02-19 15:16:31 +0300

    Add a solution to the Unite Unique problem

--- fCC-algorithms-intermediate.js
+++ fCC-algorithms-intermediate.js
@@ -269,3 +269,29 @@ function fearNotLetter(str) {
 }
 
 // console.log(fearNotLetter("abce"));
+
+/* Problem 10
+* Write a function that takes two or more arrays and returns a new array of unique values in the order
+* of the original provided arrays.
+* In other words, all values present from all arrays should be included in their original order,
+* but with no duplicates in the final array.
+* The unique numbers should be sorted by their original order,
+* but the final array should not be sorted in numerical order.
+*/
+
+/**
+ * Makes new array of unique values in the order of the original provided array.
+ * @param {Array} arrays - one or more
+ * @return {Array}
+ */
+function uniteUnique(arr) {
+    let union = [];
+    for (let i = 0; i < arguments.length; i++) {
+        for (let j = 0; j < arguments[i].length; j++) {
+            if (union.indexOf(arguments[i][j]) === -1) union.push(arguments[i][j]);
+        }
+    }
+    return union;
+}
+
+// console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
